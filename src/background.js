73c07275af49e6323122c9dabcf7bec05a132362
Fatih Kadir Akın <fakin@protel.com.tr>
2016-06-17 13:29:41 +0300

    initial commit

---
+++ src/background.js
@@ -0,0 +1,14 @@
+chrome.webRequest.onHeadersReceived.addListener(function(info) {
+  console.log(info)
+  var headers = info.responseHeaders;
+  for (var i=headers.length-1; i>=0; --i) {
+    var header = headers[i].name.toLowerCase();
+    if (header == 'x-frame-options' || header == 'frame-options') {
+      headers.splice(i, 1); // Remove header
+    }
+  }
+  return {responseHeaders: headers};
+}, {
+  urls: [ '*://salyangoz.me/*' ], // Pattern to match all http(s) pages
+  types: [ 'sub_frame' ]
+}, ['blocking', 'responseHeaders'])
